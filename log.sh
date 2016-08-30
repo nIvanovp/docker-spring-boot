441067d1a3af25ccbf544b2ea9c78d7a0a5c35d7
Nikolai Ivanov <nikolai@codefresh.io>
2016-08-30 14:46:40 +0300

    test log

--- log.sh
+++ log.sh
@@ -4,6 +4,15 @@ x=0
 while [ "$x" -lt 600 ]; do
    x=$((x+1))
    echo test log
+   echo test log
+   echo test log
+   echo test log
+   echo test log
+   echo test log
+   echo test log
+   echo test log
+   echo test log
+   echo test log
    sleep 1
 done
 
