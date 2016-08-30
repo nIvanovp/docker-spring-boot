8850dac4033f6bad896d40dd69339121010b4a8c
Nikolai Ivanov <nikolai@codefresh.io>
2016-08-30 14:27:05 +0300

    test 4

--- log.sh
+++ log.sh
@@ -1,2 +1,13 @@
-#!/bin/bash
-while true; do echo hello world; sleep 1; done
+#!/bin/sh
+
+x=0
+secs=10
+while [ "$x" -lt 10 ]; do
+   x=$((x+1))
+   echo test log
+   sleep 1
+done
+
+a=$?
+echo "tests have done"
+exit $a
