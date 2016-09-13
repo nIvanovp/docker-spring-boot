9c272430d013177efeb96ad16b6cddbd1974ab8a
nikolai <n.ivanov.p@gmail.com>
2016-09-13 16:38:51 +0300

    test3

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class Application {
     @RequestMapping("/")
     public String home() {
-        return "Hello Docker World 3";
+        return "Hello Docker World 4";
     }
 
     public static void main(String[] args) {
